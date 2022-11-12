e0eae4079c14fd94cd1a72b7722b0e340090f132
Hynesman <83984942+Hynesman@users.noreply.github.com>
2022-11-12 22:14:21 +0100

    Opdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,36 @@
 input.onButtonPressed(Button.A, function () {
+	
+})
+input.onSound(DetectedSound.Loud, function () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Orange))
     basic.showNumber(time)
     basic.showString(".")
     basic.showNumber(minut)
     basic.showString(".")
     basic.showNumber(sekund)
     basic.showString(" ")
+    strip.showColor(neopixel.colors(NeoPixelColors.Orange))
 })
+input.onButtonPressed(Button.B, function () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
+    basic.pause(5000)
+})
+let strip: neopixel.Strip = null
 let sekund = 0
 let minut = 0
 let time = 0
-time = 18
-minut = 57
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+time = 19
+minut = 0
 sekund = 30
+strip = neopixel.create(DigitalPin.P2, 8, NeoPixelMode.RGB)
+strip.showColor(neopixel.colors(NeoPixelColors.Green))
 loops.everyInterval(1000, function () {
     sekund += 1
     if (sekund == 60) {
@@ -28,7 +47,5 @@ loops.everyInterval(1000, function () {
     }
 })
 basic.forever(function () {
-    if (minut == 0 && time == 0) {
-    	
-    }
+	
 })
